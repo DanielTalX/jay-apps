28e68afbe7739f41da6d715e0f84ae20ed6fc70f
Daniel <dtal@attentigroup.com>
2024-08-23 22:55:03 +0300

    ep5 - customer module - account creation, OTP verification

--- online-food-order/online-food-order-backend-mono/src/services/ExpressApp.ts
+++ online-food-order/online-food-order-backend-mono/src/services/ExpressApp.ts
@@ -1,7 +1,8 @@
 import express , { Application } from 'express';
 import path from 'path';
 
-import { AdminRoute, VendorRoute } from '../routes'
+import { AdminRoute, VendorRoute } from '../routes';
+import { CustomerRoute } from '../routes/CustomerRoute';
 import { ShoppingRoute } from '../routes/ShoppingRoutes';
 
 
@@ -15,7 +16,8 @@ export default async(app: Application) => {
     app.use('/images', express.static(path.join(__dirname,'images')))
 
     app.use('/admin', AdminRoute);
-    app.use('/vendor', VendorRoute)
+    app.use('/vendor', VendorRoute);
+    app.use('/customer', CustomerRoute);
     app.use(ShoppingRoute);
 
     return app;
